66c402a4ab0b43dbb4f1259e87291dd831be5222
bmccartney2323 <65781099+bmccartney2323@users.noreply.github.com>
2020-11-14 12:31:00 -0500

    Completed practice with complex states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,49 @@ function App() {
     lName: "",
     email: ""
   });
+  function handleClick(event) {
+    //setHeadingText(fullName.fName + " " + fullName.lName);
+    event.preventDefault();
+  }
+  function handleChange(event) {
+    const {value, name} = event.target;
 
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } 
+      else 
+      if (name === "lName") {
+        return {
+          fName: prevValue.lName,
+          lName: value,
+          email: prevValue.email
+        };
+      } 
+      if (name === "email") {
+        return {
+          fName: prevValue.fname,
+          lName: prevValue.lName,
+          email: value
+        };
+      } 
+    });
+  }
   return (
     <div className="container">
       <h1>
         Hello {contact.fName} {contact.lName}
       </h1>
       <p>{contact.email}</p>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
+      <form onSubmit={handleClick}>
+        <input onChange={handleChange} value={contact.fName} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} value={contact.lName} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} value={contact.email} name="email" placeholder="Email" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
